bbd5b68b0cc876e83db4362d0dfc9d22ff513b83
mil1213 <tat-milakina@yandex.ru>
2022-02-01 21:40:44 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,5 +3,7 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         System.out.println("21.01,2022 ");
+        float length = 78.03256f;
+        System.out.println(length);
             }
 }
