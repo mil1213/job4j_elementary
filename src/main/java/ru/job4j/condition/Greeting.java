9675e57243ef37104f61cc97c0f6baa61e4d859c
mil1213 <tat-milakina@yandex.ru>
2022-02-03 21:17:05 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String idea1 = idea + " But I am a newbie.";
+        int year = 2022;
+        String idea2 = idea1 + year;
+        System.out.println(idea2);
+
+    }
+}
