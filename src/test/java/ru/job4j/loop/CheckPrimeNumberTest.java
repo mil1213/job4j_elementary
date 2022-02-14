5b70c90199696f518446968134d4ba17609cc7f6
Samsung <tat-milakina@yandex.ru>
2022-02-14 21:26:35 +0300

    Оператор прерывания цикла - break

---
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CheckPrimeNumberTest {
+    @Test
+    public void when5() {
+        int number = 5;
+        boolean rsl = CheckPrimeNumber.check(number);
+        Assert.assertTrue(rsl);
+    }
+
+    @Test
+    public void when4() {
+        int number = 4;
+        boolean rsl = CheckPrimeNumber.check(number);
+        Assert.assertFalse(rsl);
+    }
+
+    @Test
+    public void when1() {
+        int number = 1;
+        boolean rsl = CheckPrimeNumber.check(number);
+        Assert.assertFalse(rsl);
+    }
+}
