e8a64f8500d4d88d1f616084de540f7cc37f04b1
Samsung <tat-milakina@yandex.ru>
2022-02-15 20:48:17 +0300

    5.7. Ипотека [#156315 #264745]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        double curr = amount + amount * percent * 0.01;
-        while (curr > 0) {
+        while (amount > 0) {
         year = year + 1;
-        curr = curr - salary;
+        amount = amount * (1 + percent * 0.01) - salary;
         }
         return year;
     }
