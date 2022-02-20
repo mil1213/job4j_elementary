d9a884a343dde7b99a752b9ba8e80de2733a0116
Samsung <tat-milakina@yandex.ru>
2022-02-20 10:17:20 +0300

    6.4. Слово начинается с ... [#41585 #266924]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -6,6 +6,7 @@ public class ArrayChar {
         for (int index = 0; index < pref.length; index++) {
             if (pref[index] != word[index]) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
