64727867ca1e9b1637964596b76c6d1f641e42f4
Samsung <tat-milakina@yandex.ru>
2022-03-08 21:45:58 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,34 +7,28 @@ public class PointTest {
 
     @Test
     public void when34to51Then3dot6() {
-        int x1 = 3;
-        int y1 = 4;
-        int x2 = 5;
-        int y2 = 1;
+        Point c = new Point(3, 4);
+        Point d = new Point(5, 1);
         double exp = 3.6;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = c.distance(d);
         Assert.assertEquals(exp, out, 0.1);
     }
 
     @Test
     public void when21to62Then4dot1() {
-        int x1 = 2;
-        int y1 = 1;
-        int x2 = 6;
-        int y2 = 2;
+        Point c = new Point(2, 1);
+        Point d = new Point(6, 2);
         double exp = 4.1;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = c.distance(d);
         Assert.assertEquals(exp, out, 0.1);
     }
 
     @Test
-    public void whenm36to2mThen11dot18() {
-        int x1 = -3;
-        int y1 = 6;
-        int x2 = 2;
-        int y2 = -4;
-        double exp = 11.18;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(exp, out, 0.01);
+    public void whenm2m1tom34Then5dot1() {
+        Point c = new Point(-2, -1);
+        Point d = new Point(-3, 4);
+        double exp = 5.1;
+        double out = c.distance(d);
+        Assert.assertEquals(exp, out, 0.1);
     }
 }
